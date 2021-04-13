24ed11e17738aec6c9376e42fedfe05068a09fcd
Aofusa <5000enPCinAOFUSA@gmail.com>
2021-04-13 12:53:58 +0900

    update: hello server

---
+++ erlang/README.md
@@ -0,0 +1,15 @@
+erlang
+=====
+
+An OTP application
+
+Build
+-----
+
+    $ rebar3 compile
+
+
+Run
+-----
+
+    $ rebar3 shell
---
+++ erlang/apps/erlang/src/erlang_app.erl
@@ -0,0 +1,26 @@
+%%%-------------------------------------------------------------------
+%% @doc erlang public API
+%% @end
+%%%-------------------------------------------------------------------
+
+-module(erlang_app).
+
+-behaviour(application).
+
+-export([start/2, stop/1]).
+
+start(_StartType, _StartArgs) ->
+    Dispatch = cowboy_router:compile([
+        { <<"localhost">>, [{<<"/">>, echo_handler, []}]}    
+    ]),
+    {ok, _} = cowboy:start_clear(
+        echo_listener,
+        [{port, 8080}],
+        #{env => #{dispatch => Dispatch}}
+    ),
+    erlang_sup:start_link().
+
+stop(_State) ->
+    ok.
+
+%% internal functions
