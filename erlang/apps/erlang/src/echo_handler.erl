bf21c5ad1a9d1d848ad1a277812c2ff18c80217b
Aofusa <5000enPCinAOFUSA@gmail.com>
2021-04-13 13:50:02 +0900

    update: post echo server

--- erlang/apps/erlang/src/echo_handler.erl
+++ erlang/apps/erlang/src/echo_handler.erl
@@ -5,11 +5,28 @@
 -export([init/2]).
 
 init(Req0, State) ->
-    Req = cowboy_req:reply(
-        200,
-        #{<<"content-type">> => <<"application/json">>},
-        jsone:encode(#{<<"message">> => <<"hello,world">>}),
-        Req0
-    ),
+    Method = cowboy_req:method(Req0),
+    HasBody = cowboy_req:has_body(Req0),
+    Req = maybe_echo(Method, HasBody, Req0),
     {ok, Req, State}.
+
+maybe_echo(<<"POST">>, true, Req0) ->
+    {ok, PostVals, Req} = cowboy_req:read_urlencoded_body(Req0),
+    Echo = proplists:get_value(<<"msg">>, PostVals),
+    echo(Echo, Req);
+maybe_echo(<<"POST">>, false, Req) ->
+    cowboy_req:reply(400, [], <<"Missing body.">>, Req);
+maybe_echo(_, _, Req) ->
+    %% Method not allowed.
+    cowboy_req:reply(405, Req).
+
+echo(undefind, Req) ->
+    cowboy_req:reply(400, [], <<"Missing parameter.">>, Req);
+echo(Echo, Req) ->
+    cowboy_req:reply(
+        200,
+        #{<<"content-type">> => <<"application/json; charset=utf-8">>},
+        jsone:encode(#{<<"msg">> => Echo}),
+        Req
+    ).
 
