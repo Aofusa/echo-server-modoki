50eae0c39c67be567cce3fd9869d9c9f83514ed4
Aofusa <5000enPCinAOFUSA@gmail.com>
2021-04-14 12:37:38 +0900

    change: storage implement by actor model

--- rust/src/storage.rs
+++ rust/src/storage.rs
@@ -1,26 +1,57 @@
-use std::sync::RwLock;
-use std::collections::HashMap;
-use std::sync::Arc;
+use actix::prelude::*;
 
 use serde_derive::{Deserialize, Serialize};
 
-pub type Values = HashMap<String, String>;
+#[derive(Debug, Deserialize, Serialize, Clone)]
+pub enum Msg {
+    Set(String),
+    Get,
+}
+
+impl Message for Msg {
+    type Result = String;
+}
 
 #[derive(Debug, Deserialize, Serialize, Clone)]
-pub struct Message {
-    pub msg: String
+pub struct MsgActor {
+    pub msg: String,
 }
 
-#[derive(Clone)]
-pub struct Storage {
-    pub storage: Arc<RwLock<Values>>
+impl Actor for MsgActor {
+    type Context = Context<Self>;
 }
 
-impl Storage {
-    pub fn new() -> Self {
-        Storage {
-            storage: Arc::new(RwLock::new(HashMap::new()))
+impl Handler<Msg> for MsgActor {
+    type Result = String;
+
+    fn handle(&mut self, msg: Msg, _: &mut Context<Self>) -> Self::Result {
+        match msg {
+            Msg::Set(x) => {
+                self.msg = x;
+                self.msg.clone()
+            },
+            Msg::Get => self.msg.clone(),
         }
+    }
+}
+
+#[cfg(test)]
+mod tests_storage {
+    use actix::prelude::*;
+    use crate::storage::*;
+
+    #[actix::test]
+    async fn test_actor() {
+        let storage = MsgActor { msg: "initialize msg".to_string() }.start();
+
+        let msg = storage.send(Msg::Get).await.unwrap();
+        assert_eq!(msg, "initialize msg".to_string());
+
+        let msg = storage.send(Msg::Set("echo msg".to_string())).await.unwrap();
+        assert_eq!(msg, "echo msg".to_string());
+
+        let msg = storage.send(Msg::Get).await.unwrap();
+        assert_eq!(msg, "echo msg".to_string());
     }
 }
 
